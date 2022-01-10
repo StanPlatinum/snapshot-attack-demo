537468191cd40337324d31607ad27e6c28ef3ca1
StanPlatinum <yangzhi.lwj@antgroup.com>
2022-01-10 08:03:19 +0000

    makes the demo more reasonable

--- bash_redis/load_and_encrypt_config.c
+++ bash_redis/load_and_encrypt_config.c
@@ -3,11 +3,16 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#define BUFFER_SIZE 4096
+#include <string.h>
+#include <errno.h>
+
+#define NODE_BUFFER 4096
+#define LINE_BUFFER 1024
+
 
 int file_exists(char *filename)
 {
-   return (access(filename, 0) == 0);
+	return (access(filename, 0) == 0);
 }
 
 char * get_password(char *pass_buf) {
@@ -16,19 +21,77 @@ char * get_password(char *pass_buf) {
 	return pass_buf;
 }
 
-int set_password(int input_fd, int output_fd, char *password) {
+int set_password(char* input_filename, char* output_filename, char *password) {
+	FILE* input_fp;
+	FILE* output_fp;
+	char* line = (char *)malloc(LINE_BUFFER);
+	size_t len = 0;
+	ssize_t read;
+
+	if ((input_fp = fopen(input_filename, "r")) == NULL) {
+		printf("input file open failed!\n");
+		return -1;
+	}
+
+	if ((output_fp = fopen(output_filename, "w")) == NULL) {
+		printf("output file open failed!\n");
+		return -1;
+	}
+
+	char* strret;
+	int lines = 0;
+	while ((read = getline(&line, &len, input_fp)) != -1) {
+		lines++;
+		//search the 'requirepass' field
+		strret = strstr(line, "requirepass ");
+		if (strret != NULL) {
+			//replace the password
+			strcpy(line, "requirepass ");
+			strcat(line, password);
+			strcat(line, "\n");
+		}
+		//WL: debugging
+		if (lines > 4521 && lines < 4594) {
+			usleep(100000);
+			//WL: sleep 0.1 s
+			printf("line %d: %s", lines, line);
+		}
+
+		fwrite(line, strlen(line), 1, output_fp);
+	}
+
+	free(line);
+	fclose(input_fp);
+	fclose(output_fp);
+	return 0;
+}
+
+
+int set_password_test(char* input_file, char* output_file, char *password) {
+
+	int input_fd = open(input_file, O_RDONLY, 0600);
+	if (input_fd < 0)
+	{
+		printf("Open input file failed.\n");
+		return -1;
+	}
+	int output_fd = open(output_file, O_CREAT | O_WRONLY, 0600);
+	if (output_fd < 0)
+	{
+		printf("Open output file failed.\n");
+		return -1;
+	}
 
 	int flag;
-	char buffer[BUFFER_SIZE] = {0};
+	char buffer[NODE_BUFFER] = {0};
 	//WL: write less than 48 * 4KB data, no data would be flushed;
 	//49 * 4K does not cover the 'requirepass'
 	//50 * 4K covers the 'requirepass'
 	for (int i = 0; i < 49; i++) {
-		flag = read(input_fd, buffer, BUFFER_SIZE);
+		flag = read(input_fd, buffer, NODE_BUFFER);
 		write(output_fd, buffer, flag);
 	}
 	printf("Writing ...\n");
-
 
 	//WL: take snapshot now!
 	for (int i = 0; i < 10; i++) {
@@ -37,53 +100,34 @@ int set_password(int input_fd, int output_fd, char *password) {
 	}
 
 	//WL: write the left part
-	while ((flag = read(input_fd, buffer, BUFFER_SIZE)) > 0) {
+	while ((flag = read(input_fd, buffer, NODE_BUFFER)) > 0) {
 		//WL: read a line
 		printf("Read %d chars\n", flag);
 		write(output_fd, buffer, flag);
 	}
-
 	write(output_fd, "\0", 1);
+
+	close(input_fd);
+	close(output_fd);
 	return 0;
 }
 
 int main(void) {
 
-	int input_fd, output_fd, flag = 0;
-	char buffer[BUFFER_SIZE] = {0};
-
-	//WL: output the file at current /bin/ dir
 	char *input_file = "/etc/redis.conf.template", *output_file = "/etc/redis.conf";
+	
+	//WL: for local test
+	//char *input_file = "redis.conf.template", *output_file = "redis.conf";
 
 	//WL: check if the redis.conf exists
 	if (file_exists(output_file)) {
 		return 0;
 	}
-
-	input_fd = open(input_file, O_RDONLY, 0600);
-	if (input_fd < 0)
-	{
-		printf("Open input file failed.\n");
-		return -1;
-	}
-
-	output_fd = open(output_file, O_CREAT | O_WRONLY, 0600);
-	if (output_fd < 0)
-	{
-		printf("Open output file failed.\n");
-		return -1;
-	}
-
 	char *pass_buf = (char *)malloc(12 * sizeof(char));
 	pass_buf = get_password(pass_buf);
-	set_password(input_fd, output_fd, pass_buf);
+	set_password(input_file, output_file, pass_buf);
+	//set_password_test(input_file, output_file, pass_buf);
 
-	printf("Done.\n");
-	close(input_fd);
-	
-	//WL: do not close/flush the redis.conf in our initial demo
-	//WL: always read the whole file if we close output file
-	close(output_fd);
-
+	printf("Redis config file loaded.\n");
 	return 0;
 }
--- bash_redis/occlum_bash.sh
+++ bash_redis/occlum_bash.sh
@@ -6,14 +6,10 @@ set -xe
 busybox echo "Loading the Redis config file from a template ..."
 load_and_encrypt_config
 busybox echo "Redis config file prepared."
-busybox sleep 5
+busybox sleep 1
 
-# busybox echo "Last chance to collect the snapshot ..."
-# busybox sleep 2
-
-#WL:
-busybox echo "Checking config file ..."
-read_config
+# busybox echo "Checking config file ..."
+# read_config
 
 busybox echo "Starting up a Redis server ..."
 redis-server /etc/redis.conf --save "" --appendonly no &
